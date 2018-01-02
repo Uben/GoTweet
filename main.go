d64ed8717d177dbdb190687757de6a80272d6a07
Bernin Alejandro Uben <berninuben@icloud.com>
2018-01-01 20:37:07 -0500

    Added is_user_logged_in() to begin authorization

--- main.go
+++ main.go
@@ -1,36 +1,33 @@
-
 package main
 
 import (
 	"fmt"
+	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
 	"text/template"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
-	"golang.org/x/crypto/bcrypt"
-	"github.com/satori/go.uuid"
+	"time"
 )
 
-
 type User struct {
-    Name 	string 		  `bson:"name,omitempty"`
-    Email 	string 		  `bson:"email,omitempty"`
-    Hash 	string 		  `bson:"hash,omitempty"`
+	Name  string `bson:"name,omitempty"`
+	Email string `bson:"email,omitempty"`
+	Hash  string `bson:"hash,omitempty"`
 }
 
 type Session struct {
-	Email 	string 	`bson:"email,omitempty"`
-	Uuid 	string 	`bson:"uuid,omitempty"`
+	Email string `bson:"email,omitempty"`
+	Uuid  string `bson:"uuid,omitempty"`
 }
-
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
-
 
 func main() {
 	http.HandleFunc("/", home)
@@ -42,43 +39,41 @@ func main() {
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
-
-
 func home(res http.ResponseWriter, req *http.Request) {
 	fmt.Printf("\nUser accessed the '%s' url path.\n", req.URL.Path)
 	// tpl := template.Must(template.ParseFiles("templates/index.gohtml"))
 
-	pageData := map[string]string {
-		"Title" : "Bernin Uben | Software Developer",
-		"BodyHeader" : "Welcome to the Starting Block",
-		"Paragraph" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Suspendisse potenti nullam ac tortor vitae.",
+	_, userLoggedInInt := is_user_logged_in(req)
+
+	pageData := map[string]string{
+		"Title":          "Bernin Uben | Software Developer",
+		"BodyHeader":     "Welcome to the Starting Block",
+		"Paragraph":      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Suspendisse potenti nullam ac tortor vitae.",
+		"IsUserLoggedIn": string(userLoggedInInt),
 	}
 
 	tpl.ExecuteTemplate(res, "index.gohtml", pageData)
 }
 
-
-
-
 func register(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("\n\nUser accessed the '%s' url path.\n", r.URL.Path)
-	r.ParseForm();
+	r.ParseForm()
 	name := r.PostFormValue("name")
-    email := r.PostFormValue("email")
-    password := r.PostFormValue("password")
-    confirm_password := r.PostFormValue("confirm_password")
-    new_user := User{}
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+	confirm_password := r.PostFormValue("confirm_password")
+	new_user := User{}
 
 	if password == confirm_password {
-    	hashPass, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-    	
-    	if err != nil {
-    		log.Println(err)
-    	}
+		hashPass, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 
-    	new_user = User{name, email, string(hashPass)}
+		if err != nil {
+			log.Println(err)
+		}
 
-    	if bcrypt.CompareHashAndPassword(hashPass, []byte(password)) == nil {
+		new_user = User{name, email, string(hashPass)}
+
+		if bcrypt.CompareHashAndPassword(hashPass, []byte(password)) == nil {
 			session, err := mgo.Dial("mongodb://127.0.0.1:27017")
 			if err != nil {
 				panic(err)
@@ -87,8 +82,8 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 			col := session.DB("webdev").C("users")
 			col.Insert(new_user)
-    	}
-    }
+		}
+	}
 
 	// fmt.Printf("\nUser accessed the '%s' url path.\n", r.URL.Path)
 	// fmt.Printf("User Name: '%s'.\n", name)
@@ -96,52 +91,52 @@ func register(w http.ResponseWriter, r *http.Request) {
 	// fmt.Printf("User Password: '%s'.\n", password)
 	// fmt.Printf("User Confirm Password: '%s'.\n", confirm_password)
 
-    fmt.Printf("\nRedirecting to the '/' path\n")
+	fmt.Printf("\nRedirecting to the '/' path\n")
 	http.Redirect(w, r, "/", 303)
 }
 
-
-
-
 func login(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm();
+	r.ParseForm()
 	email := r.PostFormValue("email")
 	password := r.PostFormValue("password")
 	retUser := User{}
 
-	session, err := mgo.Dial("mongodb://127.0.0.1:27017")	
+	db, err := mgo.Dial("mongodb://127.0.0.1:27017")
+
 	if err != nil {
 		panic(err)
 	}
-	defer session.Close()
 
-	col := session.DB("webdev").C("users")
+	defer db.Close()
+	col := db.DB("webdev").C("users")
+
 	err = col.Find(bson.M{"email": email}).One(&retUser)
 
 	if err != nil {
 		panic(err)
 	}
 
-	// fmt.Println("\nResult: ", retUser)
-
+	// Compare the user hash and password
 	pwd_match := bcrypt.CompareHashAndPassword([]byte(retUser.Hash), []byte(password))
 
+	// if the password match ^ (above) ^ doesnt return a error then continue
 	if pwd_match == nil {
+		// get the "session" cookie
 		session_cookie, err := r.Cookie("session")
 
-		if err != nil {
+		if err == nil {
 			user_uuid := uuid.NewV4()
 			session_cookie = &http.Cookie{
-				Name: "session",
-				Value: user_uuid.String(),
+				Name:     "session",
+				Value:    user_uuid.String(),
 				HttpOnly: true,
 			}
 			http.SetCookie(w, session_cookie)
 
-			sCol := session.DB("webdev").C("sessions")
+			sCol := db.DB("webdev").C("sessions")
 			sCol.Insert(Session{email, user_uuid.String()})
 
-			fmt.Printf("\nUser: %s, has logged in with Session ID UUID: '%s'", retUser.Name, user_uuid)	
+			fmt.Printf("\nUser: %s, has logged in with Session ID UUID: '%s'", retUser.Name, user_uuid)
 		} else {
 			session_cookie, err := r.Cookie("session")
 			if err == nil {
@@ -157,34 +152,31 @@ func login(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 303)
 }
 
-
 func logout(w http.ResponseWriter, r *http.Request) {
-	session, err := mgo.Dial("mongodb://127.0.0.1:27017")
+	db, err := mgo.Dial("mongodb://127.0.0.1:27017")
+
 	if err != nil {
 		panic(err)
 	}
-	defer session.Close()
 
-	// Connect to the "sessions" collection in "webdev"
-	sCol := session.DB("webdev").C("sessions")
+	defer db.Close()
+	sCol := db.DB("webdev").C("sessions")
 
 	session_cookie, err := r.Cookie("session")
 
 	if err == nil {
-		// Create a temporary variable to hold a "Session" struct for later use
-		// tempUUID := Session{}
-		// Find ONE document where the "uuid" is the same as the current users session uuid value
-		// sCol.Find(bson.M{"uuid": session_cookie.Value}).One(&tempUUID)
-		// fmt.Println("\nFound the session object im about to delete: ", tempUUID)
-
 		// Remove the document where the "uuid" field is the same as the users session UUID value
-		sCol.Remove(bson.M{"uuid": session_cookie.Value })
+		sCol.Remove(bson.M{"uuid": session_cookie.Value})
 
+		// Set the cookie
 		session_cookie = &http.Cookie{
-			Name: "session",
-			Value: "",
+			Name:     "session",
+			Value:    "",
 			HttpOnly: true,
+			MaxAge:   -10,
+			Expires:  time.Now(),
 		}
+
 		http.SetCookie(w, session_cookie)
 	}
 
@@ -192,12 +184,37 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 303)
 }
 
+func is_user_logged_in(r *http.Request) (bool, int) {
+	db, err := mgo.Dial("mongodb://127.0.0.1:27017")
 
+	if err != nil {
+		panic(err)
+	}
 
+	defer db.Close()
+	sCol := db.DB("webdev").C("sessions")
 
+	session_cookie, err := r.Cookie("session")
+	retSession := Session{}
 
+	if (err == nil) && (session_cookie.Value != "") {
+		err = sCol.Find(bson.M{"uuid": session_cookie.Value}).One(&retSession)
 
+		if err == nil {
+			if retSession.Uuid == session_cookie.Value {
+				fmt.Println("\nSession: ", retSession)
+				fmt.Println("\nUser %s is logged in with session: %s.", session_cookie.Value, retSession.Email)
+				return true, 1
+			} else {
+				fmt.Println("\nUser is NOT logged in.\n")
+				return false, 0
+			}
+		} else {
+			panic(err)
+		}
 
-
-
-
+	} else {
+		fmt.Println("\nUser is NOT logged in.\n")
+		return false, 0
+	}
+}
