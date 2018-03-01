b0b571b3b632edc56dd87c349ef2e04f3ba0f7f0
Bernin Alejandro Uben <berninuben@icloud.com>
2018-02-28 22:37:41 -0500

    started switching out mongodb for postgres

--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"text/template"
 )
 
 var tpl *template.Template
+var Db *sql.DB
 
 func init() {
+	var err error
+	Db, err = sql.Open("postgres", "user=devtest dbname=gowebapp password=password sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/register", register)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/logout", logout)
+	// http.HandleFunc("/login", login)
+	// http.HandleFunc("/logout", logout)
 
 	fmt.Printf("About to listen on port :3000. Go to https://127.0.0.1:3000/ (localhost)\n")
 	log.Fatal(http.ListenAndServe(":3000", nil))
@@ -28,14 +38,14 @@ func home(res http.ResponseWriter, req *http.Request) {
 	// tpl := template.Must(template.ParseFiles("templates/index.gohtml"))
 
 	// Check if user is logged in
-	_, userLoggedInInt := is_user_logged_in(req)
+	// _, userLoggedInInt := is_user_logged_in(req)
 
 	// Create map to pass data to template
 	pageData := map[string]string{
-		"Title":          "Bernin Uben | Software Developer",
-		"BodyHeader":     "Welcome to the Starting Block",
-		"Paragraph":      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Suspendisse potenti nullam ac tortor vitae.",
-		"IsUserLoggedIn": string(userLoggedInInt),
+		"Title":      "Bernin Uben | Software Developer",
+		"BodyHeader": "Welcome to the Starting Block",
+		"Paragraph":  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna cursus eget nunc scelerisque viverra. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Suspendisse potenti nullam ac tortor vitae.",
+		// "IsUserLoggedIn": string(userLoggedInInt),
 	}
 
 	// Execute the template with the 'pageData' data
--- user.go
+++ user.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	// "database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"golang.org/x/crypto/bcrypt"
-	"gopkg.in/mgo.v2"
-	"log"
 	"net/http"
 )
 
 // Create a struct for querying User information
-// Added bson tags to allow mgo to query mongoDB
 type User struct {
-	Name  string `bson:"name,omitempty"`
-	Email string `bson:"email,omitempty"`
-	Hash  string `bson:"hash,omitempty"`
+	Id    int
+	Name  string
+	Email string
+	Hash  string
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +24,6 @@ func register(w http.ResponseWriter, r *http.Request) {
 	email := r.PostFormValue("email")
 	password := r.PostFormValue("password")
 	confirm_password := r.PostFormValue("confirm_password")
-	new_user := User{}
 
 	// if 'password' && 'confirm_password' have the same value
 	if password == confirm_password {
@@ -32,28 +31,15 @@ func register(w http.ResponseWriter, r *http.Request) {
 		hashPass, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 
 		if err != nil {
-			log.Println(err)
+			panic(err)
 		}
 
-		// Create the User struct were going to add into database
-		new_user = User{name, email, string(hashPass)}
+		// insert the user into the users table in postgres
+		_, nErr := Db.Exec("insert into users (name, email, password) values ($1, $2, $3)", name, email, hashPass)
 
-		/* if the hash we created and we take the same password and make it
-		to a hash had the same original value, this should be true */
-		if bcrypt.CompareHashAndPassword(hashPass, []byte(password)) == nil {
-			// Create a connection to the database
-			conn, err := mgo.Dial("mongodb://127.0.0.1:27017")
-			// Check of there is an error connecting to the database
-			if err != nil {
-				panic(err)
-			}
-			// Defer closing the connection to the end of this function
-			defer conn.Close()
-
-			// Connect to the database "webdev" and access the "users" collection
-			col := conn.DB("webdev").C("users")
-			// Insert the user struct, previously created, into the database
-			col.Insert(new_user)
+		// Check of there is an error connecting to the database
+		if nErr != nil {
+			panic(nErr)
 		}
 	}
 
